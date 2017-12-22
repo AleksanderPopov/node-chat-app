a6f0e3b9774e6b0af2b859203bd90d202a4c722b
Alexander Popov <aleksander.popov.93@gmail.com>
2017-12-22 18:46:46 +0200

    add socket.io

--- server/app.ts
+++ server/app.ts
@@ -1,8 +1,24 @@
 import * as express from 'express';
 import * as path from 'path';
+import * as http from "http";
+import * as sockerIO from 'socket.io';
+
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 
 const app = express();
-const port = process.env.PORT || 3000;
-app.use(express.static(path.join(__dirname, '../public')));
+const httpServer = http.createServer(app);
+const io = sockerIO(httpServer);
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+app.use(express.static(publicPath));
+
+io.on('connection', (socket) => {
+    console.log('New user connected!');
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected!');
+    });
+});
+
+
+httpServer.listen(port, () => console.log(`Server started on port ${port}`));
